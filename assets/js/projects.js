2981e0112f6b2871afb50dd256e52c5beacefb32
Maureen Durnin <maureenmdurnin@gmail.com>
2019-12-06 14:35:26 -0800

    remove prev and next buttons from project slides temp

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -7,20 +7,17 @@ clickedPage.addEventListener("click", (e) => {
     start = document.getElementById("start")
     end = document.getElementById("end")
 
-    // (e.target.innerHTML === "1") ?
-    //     // start.className = "page disabled start" : start.className = "page start"
-    //     console.log("left") : console.log("right")
-    if (e.target.innerHTML === "1") {
-        start.className = "button disabled start"
-    } else {
-        start.className = "button start"
-    }
+    // if (e.target.innerHTML === "1") {
+    //     start.className = "button disabled start"
+    // } else {
+    //     start.className = "button start"
+    // }
 
-    if (e.target.innerHTML === "3") {
-        end.className = "button disabled end"
-    } else {
-        end.className = "button end"
-    }
+    // if (e.target.innerHTML === "3") {
+    //     end.className = "button disabled end"
+    // } else {
+    //     end.className = "button end"
+    // }
 
 
     previous = clickedPage.querySelector(".active")
@@ -31,10 +28,7 @@ clickedPage.addEventListener("click", (e) => {
     prevImg.parentNode.classList.remove("revealed")
     prevImg.parentNode.classList.add("concealed")
 
-    // if (e.target.innerHTML === "Next") {
-    //     targetImgId = displayHash[`${e.target.innerHTML}`]
-    //     targetImg = projectDoc.querySelector(`.${targetImgId}`)
-    // }
+
     targetImgId = displayHash[`${e.target.innerHTML}`]
     targetImg = projectDoc.querySelector(`.${targetImgId}`)
 
