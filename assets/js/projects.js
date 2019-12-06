c28bc3a9e029d69e2c744a97f69223e4fbd3f072
Maureen Durnin <maureenmdurnin@gmail.com>
2019-12-06 01:09:04 -0800

    update prev and next pagination button classes

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -1,11 +1,28 @@
 const clickedPage = document.getElementById("pagination")
 const projectDoc = document.getElementById("projects-head")
-const test = projectDoc.querySelector("three")
-console.log(!!test)
 
 displayHash = {"1": "one", "2": "two", "3": "three"}
 
 clickedPage.addEventListener("click", (e) => {
+    start = document.getElementById("start")
+    end = document.getElementById("end")
+
+    // (e.target.innerHTML === "1") ?
+    //     // start.className = "page disabled start" : start.className = "page start"
+    //     console.log("left") : console.log("right")
+    if (e.target.innerHTML === "1") {
+        start.className = "button disabled start"
+    } else {
+        start.className = "button start"
+    }
+
+    if (e.target.innerHTML === "3") {
+        end.className = "button disabled end"
+    } else {
+        end.className = "button end"
+    }
+
+
     previous = clickedPage.querySelector(".active")
     prevImgId = displayHash[`${previous.innerHTML}`]
     prevImg = projectDoc.querySelector(`.${prevImgId}`)
@@ -14,12 +31,14 @@ clickedPage.addEventListener("click", (e) => {
     prevImg.parentNode.classList.remove("revealed")
     prevImg.parentNode.classList.add("concealed")
 
+    // if (e.target.innerHTML === "Next") {
+    //     targetImgId = displayHash[`${e.target.innerHTML}`]
+    //     targetImg = projectDoc.querySelector(`.${targetImgId}`)
+    // }
     targetImgId = displayHash[`${e.target.innerHTML}`]
     targetImg = projectDoc.querySelector(`.${targetImgId}`)
 
     targetImg.parentNode.classList.remove("concealed")
     targetImg.parentNode.classList.add("revealed")
     e.target.classList.add("active")
-
-    if 
 })
