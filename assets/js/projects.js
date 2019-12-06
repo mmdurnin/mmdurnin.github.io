9ad86c3e3b0784fd61e5474025a5a29cfc8a9917
Maureen Durnin <maureenmdurnin@gmail.com>
2019-12-06 00:30:39 -0800

    update pagination with images

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -1,7 +1,25 @@
 const clickedPage = document.getElementById("pagination")
+const projectDoc = document.getElementById("projects-head")
+const test = projectDoc.querySelector("three")
+console.log(!!test)
+
+displayHash = {"1": "one", "2": "two", "3": "three"}
 
 clickedPage.addEventListener("click", (e) => {
     previous = clickedPage.querySelector(".active")
+    prevImgId = displayHash[`${previous.innerHTML}`]
+    prevImg = projectDoc.querySelector(`.${prevImgId}`)
+
     previous.classList.remove("active")
+    prevImg.parentNode.classList.remove("revealed")
+    prevImg.parentNode.classList.add("concealed")
+
+    targetImgId = displayHash[`${e.target.innerHTML}`]
+    targetImg = projectDoc.querySelector(`.${targetImgId}`)
+
+    targetImg.parentNode.classList.remove("concealed")
+    targetImg.parentNode.classList.add("revealed")
     e.target.classList.add("active")
+
+    if 
 })
